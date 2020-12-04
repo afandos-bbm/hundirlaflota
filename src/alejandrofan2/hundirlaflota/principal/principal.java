de1839fbc004db39c062f143993a349eb94898d6
Alejandro <alejandrofan2@gmail.com>
2020-12-04 13:57:46 +0100

    Se ha creado el JFrame principal y a su vez dos clases con sus JPanel correspondientes. Ademas se ha incluido en el paquete alejandrofan2.hundirlaflota.multimedia la foto del logo. Sigo con la estructura de el JForm para la visualización correcta del programa.

--- src/alejandrofan2/hundirlaflota/principal/principal.java
+++ src/alejandrofan2/hundirlaflota/principal/principal.java
@@ -1,9 +1,31 @@
 package alejandrofan2.hundirlaflota.principal;
 
-public class principal {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+ 	
+public class principal extends JFrame {
+	
+	static menu menu = new menu();
+	
+	static String rutaLogo = "/home/alefer/Documentos/logo.png";
+    static JFrame juego = new JFrame("Hundir la flota (Singleplayer)");
+	int posicionesMapa[][] = new int[25][25];
 
 	public static void main(String[] args) {
-		
+		panel();
+	}
+
+	
+	public static void panel() {
+		 menu menu = new menu();
+		 cabecera cabecera = new cabecera();
+	     juego.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
+	     juego.setSize(800, 600);
+	     juego.setLayout(new BorderLayout());
+	     juego.add(menu.menu(), BorderLayout.EAST);
+	     juego.add(cabecera.cabecera(), BorderLayout.NORTH);
+	     juego.setVisible(true);
 	}
 
 }
