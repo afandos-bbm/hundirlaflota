5d334cef10375f1981f61888f0e3ada3ae0d3b9e
Alejandro Fandos <alejandrofan2@gmail.com>
2020-12-11 13:55:45 +0100

    Pruebas mapa
    Seguimos realizando pruebas en el creador del mapa

--- src/alejandrofan2/hundirlaflota/principal/MecanicasJuego.java
+++ src/alejandrofan2/hundirlaflota/principal/MecanicasJuego.java
@@ -3,7 +3,9 @@ package alejandrofan2.hundirlaflota.principal;
 public class MecanicasJuego {
 
 	private static int nIntentos = 50;
-	private static int tamañoMapa = 11;
+	private static int tamañoMapa = 10;
+	private static int[] ejeX;
+	private static char[] ejeY;
 	private static char[][] MapaJugador;
 	private static char[][] MapaContrincante;
 
@@ -15,31 +17,24 @@ public class MecanicasJuego {
 
 	public static char[][] crearmapa(int tamaño) {
 		char[][] m = new char[tamaño][tamaño];
-		int cont = -1;
-		int cont2 = 0;
+		ejeX = new int[tamaño];
+		ejeY = new char[tamaño];
 		for (int i = 0; i < m.length; i++) {
+			int cont = 65;
+			ejeX[i] = i;
+			ejeY[i] = (char) cont;
+			cont++;
 			for (int j = 0; j < m.length; j++) {
 				m[i][j] = 45;
-				if (i == 0) {
-					m[i][j] = (char) (65 + cont);
-					cont++;
-				} else if (j == 0) {
-					if (cont2 < 10) {
-						m[i][j] = (char) (48 + cont2);
-					} else if (cont2 >= 10 && cont2 < 20) {
-						m[i][j] = (char) (29 + cont2);
-					}
-					cont2++;
 				}
 			}
-		}
 		Barcos.insertarBarcos(m, Barcos.getnLanchas(), Barcos.getnBuques(), Barcos.getnAcorazado(),
 				Barcos.getnPortaaviones());
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m.length; j++) {
 				System.out.print(m[i][j] + "  ");
 			}
-			System.out.println("");
+			System.out.println(ejeY + " | ");
 		}
 		return m;
 	}
